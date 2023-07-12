629f3b094db15d3f201c99cbbb98aeb09bf0e719
james <ejmelias@gmail.com>
2023-07-12 09:20:50 -0500

    improved layout with mobile nav

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,8 @@ import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
 import Background from '@/components/Background'
+import MobileNav from '@/components/MobileNav'
+import Nav from '@/components/Nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,94 +19,33 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <main className="flex min-h-screen flex-col items-center justify-between">
-
+                <main className="flex flex-col min-h-screen items-center justify-start">
+                    
                     <Background />
-
-                    {/*Title*/}
-                    <div className="border border-pink-600 rounded-md z-10 w-full max-w-5xl items-center justify-center flex">
-                        <div className="flex border border-green-600 rounded-md items-center justify-center bg-gradient-to-b from-white via-white dark:from-black dark:via-black static h-auto w-auto lg:bg-none">
+                    
+                    <div className=" z-20 h-full w-full max-w-5xl flex-col">
+                        {/*Title*/}
+                        <div className="flex md:justify-center justify-between items-center bg-gradient-to-b from-white via-white dark:from-black dark:via-black static md:bg-none">
                             <Link
-                                className="flex place-items-center gap-2 p-8 pointer-events-auto lg:p-0"
+                                className="flex p-6 pointer-events-auto md:p-0"
                                 href="/"
                             >
-                                <h1 className='text-5xl lg:text-9xl'>R3F + Next.js</h1>
+                                <h1 className='text-5xl md:text-8xl lg:text-9xl'>R3F + Next.js</h1>
                             </Link>
+                            <MobileNav />
+                        </div>                 
+
+                        <Nav />
+
+                    </div>
+                    
+                    {/*content*/}
+                    <div className='z-10 max-w-5xl md:flex-auto flex items-center justify-center h-screen absolute'>
+                        <div className='rounded-md bg-gray-100/75 p-5'>
+                            {children}
                         </div>
                     </div>
                     
-
-                    
-                    {/*Navigation*/}
-                    <div className="border border-orange-600 rounded-md z-10 mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-                        <Link href={'/about'} className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30">
-                            <h2 className={`mb-3 text-2xl font-semibold`}>
-                                About{' '}
-                                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                                    -&gt;
-                                </span>
-                            </h2>
-                            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                                Find in-depth information about Next.js features and API.
-                            </p>
-                        </Link>
-
-                        <a
-                            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <h2 className={`mb-3 text-2xl font-semibold`}>
-                                Learn{' '}
-                                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                                    -&gt;
-                                </span>
-                            </h2>
-                            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                                Learn about Next.js in an interactive course with&nbsp;quizzes!
-                            </p>
-                        </a>
-
-                        <a
-                            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <h2 className={`mb-3 text-2xl font-semibold`}>
-                                Templates{' '}
-                                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                                    -&gt;
-                                </span>
-                            </h2>
-                            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                                Explore the Next.js 13 playground.
-                            </p>
-                        </a>
-
-                        <a
-                            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <h2 className={`mb-3 text-2xl font-semibold`}>
-                                Deploy{' '}
-                                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                                    -&gt;
-                                </span>
-                            </h2>
-                            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                                Instantly deploy your Next.js site to a shareable URL with Vercel.
-                            </p>
-                        </a>
-                    </div>
-
-                    {/*content*/}
-                    <div className='z-10 border border-blue-600 rounded-md'>
-                        {children}
-                    </div>
                 </main>
             </body>
         </html>
