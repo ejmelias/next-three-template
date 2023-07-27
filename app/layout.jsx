8cd84403113ea941252ad0f3997d9635566610e3
james <ejmelias@gmail.com>
2023-07-26 21:05:35 -0500

    fixed ios address bar jump, added suspense to canvas

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <main className="flex flex-col min-h-screen items-center ">
+                <main className="flex flex-col min-h-full items-center ">
                     
                     <Background />
                     
